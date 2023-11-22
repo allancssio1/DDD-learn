d44e92cf66cafe8ae2b0198c26444b2032ca56f5
allancassio <allan.cassio1@gmail.com>
2023-11-22 18:45:00 -0300

    commit to review

--- src/domain/useCases/answerQuestion.spec.ts
+++ src/domain/useCases/answerQuestion.spec.ts
@@ -1,4 +1,3 @@
-import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 import { AnswerQuestionUseCase } from './answerQuestion'
 import { AnswerRepository } from '../repositorues/answerRepository'
 import { Answer } from '../entities/Answer'
@@ -9,16 +8,12 @@ const fakeAnswerRepository: AnswerRepository = {
   },
 }
 
-describe('', () => {
-  beforeEach(() => {})
-  afterEach(() => {})
-  it('should be create a new asnwer', async () => {
-    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
-    const answer = await answerQuestion.execute({
-      instructorId: '1',
-      questionId: '2',
-      content: 'nova resposta',
-    })
-    expect(answer.content).toEqual('nova resposta')
+test('should be create a new asnwer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+  const answer = await answerQuestion.execute({
+    instructorId: '1',
+    questionId: '2',
+    content: 'nova resposta',
   })
+  expect(answer.content).toEqual('nova resposta')
 })
