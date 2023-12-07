c90d1a9796fb4adc93bce3c701e07424f1027525
allancassio <allan.cassio1@gmail.com>
2023-12-07 05:17:09 -0300

    create functionality find many questions

--- src/domain/forum/apllication/repositories/questionsRepository.ts
+++ src/domain/forum/apllication/repositories/questionsRepository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/paginationParams'
 import { Question } from '../../enterprise/entities/Question'
 
 export interface QuestionsRepository {
@@ -5,5 +6,6 @@ export interface QuestionsRepository {
   findBySlug(slug: string): Promise<Question | null>
   delete(question: Question): Promise<void>
   save(question: Question): Promise<void>
+  findManyRecent(props: PaginationParams): Promise<Question[]>
   findById(questionId: string): Promise<Question | null>
 }
