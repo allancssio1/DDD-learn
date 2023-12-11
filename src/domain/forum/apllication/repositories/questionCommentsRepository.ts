98b27f63025b19e0b02e9bd1b2631a291d9132fd
allancassio <allan.cassio1@gmail.com>
2023-12-11 05:26:55 -0300

    implement functionality delete and findById to repository contract

--- src/domain/forum/apllication/repositories/questionCommentsRepository.ts
+++ src/domain/forum/apllication/repositories/questionCommentsRepository.ts
@@ -2,4 +2,6 @@ import { QuestionComment } from '../../enterprise/entities/questionComment'
 
 export interface QuestionCommentsRepository {
   create(questionComment: QuestionComment): Promise<void>
+  findById(questionCommentId: string): Promise<QuestionComment | null>
+  delete(questionComment: QuestionComment): Promise<void>
 }
