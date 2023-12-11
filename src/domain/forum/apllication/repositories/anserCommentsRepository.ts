3b777879229762fa0ca6586bf531204a5dfca97d
allancassio <allan.cassio1@gmail.com>
2023-12-11 05:42:54 -0300

    implement functionality delete and findById to repository contract

--- src/domain/forum/apllication/repositories/anserCommentsRepository.ts
+++ src/domain/forum/apllication/repositories/anserCommentsRepository.ts
@@ -2,4 +2,6 @@ import { AnswerComment } from '../../enterprise/entities/answerComment'
 
 export interface AnswerCommentsRepository {
   create(answerComment: AnswerComment): Promise<void>
+  findById(answerCommentId: string): Promise<AnswerComment | null>
+  delete(answerComment: AnswerComment): Promise<void>
 }
