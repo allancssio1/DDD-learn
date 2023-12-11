02798502ff28c9261c8cf48e1a2172a2974c9241
allancassio <allan.cassio1@gmail.com>
2023-12-11 05:41:51 -0300

    create test delete answer comment use case

---
+++ src/domain/forum/apllication/useCases/deleteAnswerComment.spec.ts
@@ -0,0 +1,46 @@
+import { InMemoryAnswerCommentsReposiroty } from '@/test/repositories/inMemoryAnswerCommentsRepository'
+import { DeleteAnswerCommentUseCase } from './deleteAnswerComment'
+import { UniqueEntityId } from '@/core/entities/uniqueEntityId'
+import { makeAnswerComment } from '@/test/factories/makeAnswerComment'
+
+let inMemoryAnswerCommentRepository: InMemoryAnswerCommentsReposiroty
+let sut: DeleteAnswerCommentUseCase
+describe('Delete Answer Comment', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentRepository = new InMemoryAnswerCommentsReposiroty()
+    sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentRepository)
+  })
+
+  test('should be delete answer comment', async () => {
+    const newAnswerComment = makeAnswerComment(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('answer-1'),
+    )
+    await inMemoryAnswerCommentRepository.create(newAnswerComment)
+    await sut.execute({
+      authorId: newAnswerComment.authorId.toString(),
+      questoinCommentId: newAnswerComment.id.toString(),
+    })
+    expect(inMemoryAnswerCommentRepository.items).toHaveLength(0)
+  })
+
+  test('should not be delete answer comment if action author diff answer comment author', async () => {
+    const newAnswerComment = makeAnswerComment(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('answer-1'),
+    )
+    await inMemoryAnswerCommentRepository.create(newAnswerComment)
+
+    expect(
+      async () =>
+        await sut.execute({
+          authorId: 'author-2',
+          questoinCommentId: newAnswerComment.id.toString(),
+        }),
+    ).rejects.toThrow(new Error('Not allowed'))
+  })
+})
