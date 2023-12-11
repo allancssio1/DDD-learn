ed344a1ffe103888860efb772918fe0128c69a5a
allancassio <allan.cassio1@gmail.com>
2023-12-11 05:40:35 -0300

    create functionality delete answer comment

--- src/test/repositories/inMemoryAnswerCommentsRepository.ts
+++ src/test/repositories/inMemoryAnswerCommentsRepository.ts
@@ -5,7 +5,23 @@ export class InMemoryAnswerCommentsReposiroty
   implements AnswerCommentsRepository
 {
   items: AnswerComment[] = []
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
+  }
+
+  async findById(answerCommentId: string): Promise<AnswerComment | null> {
+    const answerComment = await this.items.find(
+      (item) => item.id.toString() === answerCommentId,
+    )
+    return answerComment ?? null
+  }
+
+  async delete(answerComment: AnswerComment): Promise<void> {
+    const itemIndex = await this.items.findIndex(
+      (item) => item.id.toString() === answerComment.id.toString(),
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
